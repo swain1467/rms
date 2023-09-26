b0d86f0a688eb78a11a5124e6f3a3fb4407d1c3f
swain1467 <swain1467@gmail.com>
2023-09-26 21:18:30 +0530

    code refactorize

--- app/Http/Controllers/admin/SetupController.php
+++ app/Http/Controllers/admin/SetupController.php
@@ -7,11 +7,14 @@ use App\Models\City;
 use App\Models\Area;
 use App\Models\Type;
 
+
+use App\Service\SetupService;
+
 class SetupController extends Controller
 {
-    public function __construct()
-    {
-        // $this->middleware('auth:api');
+    public function __construct(
+        private readonly SetupService $setupService
+    ) {
     }
 
     public function SetupView(){
@@ -197,69 +200,16 @@ class SetupController extends Controller
     }
 //API Data-------------------------------------- 
     public function GetCityListAPI(Request $request){
-        $output = array('status' => '', 'aaData[]' => array(), 'aaData1[]' => array());
-        
-        $city = City::select('id', 'city_name', 'status')
-                ->orderBy('city_name','ASC')
-                ->get();
-
-        $data = $city->toArray();
-        $slno = 1; 
-        foreach($data as $row){  
-            $row['sl_no'] = $slno;
-            $output['aaData'][] = $row;
-            $output['status'] = 'Success';
-            $slno++;
-        }
-        
-        $city = City::select('id', 'city_name')
-                ->where('status', 1)
-                ->orderBy('city_name','ASC')
-                ->get();
-
-        $data = $city->toArray();
-        foreach($data as $row){  
-            $output['aaData1'][] = $row;
-        }
-        return $output;
+        return $this->setupService->GetCityListAPI();
     }
 
     public function SaveCityAPI(Request $request){
-        $output = array('status' => '', 'message' => '');
-        
-        if(!$request['city_name']){
-            $output['status'] = 'Error';
-            $output['message'] = 'City/Town is required';
+        $output = $this->setupService->validateCityData($request);
+        if(!$output){
+            return $this->setupService->saveCityData($request);
+        }else{
             return $output;
         }
-        
-        if($request['city_id'] == ''){
-            $city = new City;
-            $city->city_name = $request['city_name'];
-            $city->contact_person = $request['id'];
-            $city->status = $request['city_status'];
-            $city->contact_on = '7846993676';
-            $city->created_at = date("Y-m-d H:i:s");
-            $city->created_by = $request['id'];
-            $city->updated_at = date("Y-m-d H:i:s");
-            $city->updated_by = $request['id'];
-            $city->save();
-            $output['status'] = 'Success';
-            $output['message'] = 'City  added successfully';
-        }else{
-            $update = City::where("id", $request['city_id'])->
-            update(["city_name" => $request['city_name'], "status" => $request['city_status'],
-                    "updated_by" => $request['id'], "updated_at" => date("Y-m-d H:i:s")]);
-            
-            if($update == 1){
-                $output['status'] = 'Success';
-                $output['message'] = 'City  updated successfully';
-            }else{
-                $output['status'] = 'Failure';
-                $output['message'] = 'Oops! Something went wrong';
-            }
-        }
-       return $output;
     }
 
     public function GetAreaListAPI(Request $request){
